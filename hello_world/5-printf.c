a5423be0b4a105f343e6013ef9fd03ba9603c2d0
TessierV <5628@holbertonstudents.com>
2022-09-28 05:46:03 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *Your program should Return: 0        
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
