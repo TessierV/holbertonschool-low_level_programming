03e1daee5c81e59476a838e3769117f391181220
TessierV <5628@holbertonstudents.com>
2022-09-28 06:30:25 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints \"Programming is like building a multilingual puzzle\"
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	printf("Size of a char: %d byte(s) \n", sizeof(char));	
+	printf("Size of an int: %d byte(s) \n", sizeof(short int));
+	printf("Size of a long int: %ld byte(s) \n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %d byte(s) \n", sizeof(float));
+		return (0);
+}
