1be59f4d99083b85df399642e05cc829b5923b47
TessierV <5628@holbertonstudents.com>
2022-09-28 07:21:49 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,6 +8,6 @@
  **/
 int main(void)
 {
-		write("with proper grammar, but the outcome is a piece of art,\n");
+		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 			return (0);
 }
