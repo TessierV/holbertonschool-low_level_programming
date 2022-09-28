72f19345d8b161ff958681183583eebfc21e242c
TessierV <5628@holbertonstudents.com>
2022-09-28 05:33:42 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-/*Write a C program that prints exactly \"Programming is like building a multilingual puzzle, followed by a new line.*/
-int main(void)
+/**
+ * Hello,puts 
+ *
+ */
+main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
