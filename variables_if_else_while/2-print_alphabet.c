5cc039326d73b20edff8269b66fab2a4591e3d98
TessierV <5628@holbertonstudents.com>
2022-10-03 05:13:15 -0700

    Creating a script for know if int is negative or positive

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - determine if alphabet
+ *
+ * Description: program will assign a random letter the variable n each time
+ * it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i= 'a';
+
+	for(i= 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
