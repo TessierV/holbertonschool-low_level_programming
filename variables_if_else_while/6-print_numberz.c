5e8cf3aaf2c4c1e252b2502b820b2b4975904c67
TessierV <5628@holbertonstudents.com>
2022-10-03 05:56:47 -0700

    Creating a script for print all single digit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print all single digit numbers of base 10
+ *
+ * Description: with a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
