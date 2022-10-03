7cfcd3fda1a4484963ad9c2a04471ed07e02f9b4
TessierV <5628@holbertonstudents.com>
2022-10-03 04:46:45 -0700

    Creating a script for know if int is negative or positive

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@ int main(void)
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 				lastint = n % 10;
-				if (lastint < 6 && lastint != 0)
-					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastint);
+				if (lastint > 5)
+					printf("Last digit of %d is %d and is greater than 5\n", n, lastint);
 				else if (lastint == 0)
 					printf("Last digit of %d is %d and is 0\n", n, lastint);
-				else if (lastint > 5)
-					printf("Last digit of %d is %d and is greater than 5\n", n, lastint);
+				else if (lastint < 6 && lastint != 0)
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastint);
 				return (0);
 }
