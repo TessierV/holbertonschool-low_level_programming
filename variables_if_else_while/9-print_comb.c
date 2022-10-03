c3983834fff9b24124ffa0406e008513ffa8120b
TessierV <5628@holbertonstudents.com>
2022-10-03 06:32:39 -0700

    Creating a script for print all single digit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print all single aphabet in reverse
+ *
+ * Description: with a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('%d, \n', n);
+	return (0);
+}
