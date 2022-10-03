6112a6120ae8ee99b9dbde4a43a5842ff7af1f64
TessierV <5628@holbertonstudents.com>
2022-10-03 06:07:52 -0700

    Creating a script

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print all single aphabet in reverse
+ *
+ * Description: with a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i <= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
