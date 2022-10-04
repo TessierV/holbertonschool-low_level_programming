88a671f9dac559af50c2c6e7b6245a2b774ea03d
TessierV <5628@holbertonstudents.com>
2022-10-04 09:23:45 -0700

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	_putchar(95);
-	_putchar(80);
-	_putchar(85);
-	_putchar(84);
-	_putchar(67);
-	_putchar(72);
-	_putchar(65);
-	_putchar(82);
+	_putchar(75);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 	return(0);
 }
