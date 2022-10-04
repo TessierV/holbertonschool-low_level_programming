ee5fe3c5b4b77dfd58d402107f9ffc4eaec421ad
TessierV <5628@holbertonstudents.com>
2022-10-04 09:50:00 -0700

    script for 1-alphabet.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ * main - print_alphabet a lowercase alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int y = 0;	
+	char i = 'a';
+
+	for (y = 0; y <= 9; y++)
+	{ 
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+			putchar('\n');
+			}
+	}
+	return (0);
+}
