ec2bdf4b14cf493a7ef87f29fae8c4990a8ce801
TessierV <5628@holbertonstudents.com>
2022-10-04 06:07:41 -0700

    reajustment

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_alphabet a lowercase alphabet
+ *
+ * Return: void
+ * */
+void print_alphabet(void)
+{
+		char i;
+
+			for (i = 'a'; i <= 'z'; i++)
+						putchar(i);
+				putchar('\n');
+}
