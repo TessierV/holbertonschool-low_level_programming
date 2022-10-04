5199cb3725703993f5be90588d30c25ae71d138b
TessierV <5628@holbertonstudents.com>
2022-10-04 04:13:36 -0700

    creating script 0 and files

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print Putchar
+ *
+ * Description: with a new line and ascii table
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	putchar(95);
+	putchar(80);
+	putchar(85);
+	putchar(84);
+	putchar(67);
+	putchar(72);
+	putchar(65);
+	putchar(82);
+}
