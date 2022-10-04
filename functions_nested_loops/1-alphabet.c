2a535796cc72c5a028ac623f6030a9b055402a1b
TessierV <5628@holbertonstudents.com>
2022-10-04 12:24:39 -0700

    script for 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0.
  */
-char print_alphabet(void)
+void print_alphabet(void)
 {
 	char i = 'a';
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MY_HEADER_H
 
 int _putchar(char c);
-char print_alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
