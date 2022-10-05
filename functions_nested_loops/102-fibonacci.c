c9d0db267a43d5d32fd9f8e032197e300f9b5a89
TessierV <5628@holbertonstudents.com>
2022-10-05 06:31:08 -0700

    fibo

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ *_abs - check is the character is an alphabetic
+ *@r: its the name of the variable
+ * Return: 1 if true 0 if false
+**/
+int fibo(void)
+{
+	int a, b, c, fi;
+	a = 1;
+	b = 2;
+	printf("%d","%d", a, b);
+	for (fi = 0; fi < 50; fi++)
+	{
+		c = a + b;
+		printf("%d, fn");
+	}
+}
