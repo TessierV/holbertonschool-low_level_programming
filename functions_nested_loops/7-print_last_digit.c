ab311ffe6c15c58bfebb92012a2281eaaa96a7e2
TessierV <5628@holbertonstudents.com>
2022-10-05 04:43:43 -0700

    test 7 last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,10 @@
 int print_last_digit(int n)
 {
 	int r;
-	_putchar(r(n % 10) + '0');
-	return(r);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return(0);
 }
