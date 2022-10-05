a05e4c44686ef4f49b36520f6a0cf2a0bd8d227e
TessierV <5628@holbertonstudents.com>
2022-10-05 03:32:47 -0700

    test 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,11 +14,7 @@ void times_table(void)
 		{
 			int result = c * n;
 
-			if (result == 0)
-			{
-				_putchar('0');
-			}
-			else if (result <= 9)
+			if (result <= 9)
 			{
 				_putchar(44);
 				_putchar(32);
