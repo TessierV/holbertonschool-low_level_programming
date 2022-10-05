d5d85fea16d60c89180460f0fbe501fd23a2695c
TessierV <5628@holbertonstudents.com>
2022-10-05 02:04:42 -0700

    test 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,23 +1,18 @@
 #include "main.h"
 /**
- *print_times_table - multiplication tab
+ *times_table - multiplication tab
  *@n: its the name of the variable
  * Return: \n
  */
 void times_table(void)
 {
-	int c, r, n;
+	int c, n;
 
 	for (c = 0; c <= n; c++)
 	{
-		for (r = 0; r <= n; r++)
-		{
-			int result = c * r;
-			_putchar(result + '0');
-			_putchar(',');
-			_putchar(' ');
-			}
-			putchar('\n');
-		}
+		int result = c * n;
+		_putchar(result + '0');
+		_putchar(',');
+		_putchar(' ');
 	}
 }
