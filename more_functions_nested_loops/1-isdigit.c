6951a10a84d3fc4a53366501c34aac34c608c69e
TessierV <5628@holbertonstudents.com>
2022-10-06 09:55:57 -0700

    script for digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - is uppercase
+ * _isdigit - is Digital
  * @c: its the variable
  *
- *Description: if its an uppercase print 1
+ *Description: if its a number print 1
  *Return: 1 when true return false when 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
