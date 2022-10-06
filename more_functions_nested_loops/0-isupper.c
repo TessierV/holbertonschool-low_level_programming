2e76ec95f6f22110f87127fa611a1120d1ac272b
TessierV <5628@holbertonstudents.com>
2022-10-06 09:50:18 -0700

    adjusting betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,17 @@
  * _isupper - transform character in uppercase
  * @c: its the variable
  *
- *Description: to the lowercase into uppercase
+ *Description: if its an uppercase print 1
+ *Return: 1 when true return false when 0
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
-	} 
+	}
 	else
 	{
 		return (0);
 	}
-}
+}i
