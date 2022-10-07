96e7781799073f47bcba09e79f89421de9b4e6a8
TessierV <5628@holbertonstudents.com>
2022-10-07 04:29:18 -0700

    testing 9 buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,10 +9,9 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 101; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (x != 100)
-		{
+		
 			if (x % 3 == 0 && x % 5 == 0)
 			{
 				printf("Fizz Buzz");
@@ -29,8 +28,11 @@ int main(void)
 			{
 				printf("%d", x);
 			}
+			if (x != 100)
+			{
 			printf(" ");
-		}	
+			}
+	
 	}
 	return (0);
 }
