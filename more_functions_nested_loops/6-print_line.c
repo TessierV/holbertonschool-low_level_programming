00890572d03be917bb86699cf4b8251de0fbc07f
TessierV <5628@holbertonstudents.com>
2022-10-07 01:56:07 -0700

    testing 6lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_functions_nested_loops - print ligne
+ *@n: is the variable
+ *
+ *Description: print a ligne with a variable
+ *Return: /n
+ */
+void print_line(int n)
+{
+	int _;
+
+	for (_ = 0; _ <= n; _++)
+	{
+		putchar(58);
+	}
+	putchar('/n');
+}
