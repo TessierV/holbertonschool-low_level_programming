31606161703c342ca76f947c14f84daf25dc4263
TessierV <5628@holbertonstudents.com>
2022-10-07 04:18:12 -0700

    testing 9 buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,25 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if (x != 100)
 		{
-			printf("Fizz Buzz ");
-		}
-		else if (x % 3 == 0)
-		{
-			printf("Fizz "); 
-		}
-		else if (x % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", x);
+			printf(" ");
+			if (x % 3 == 0 && x % 5 == 0)
+			{
+				printf("Fizz Buzz");
+			}
+			else if (x % 3 == 0)
+			{
+				printf("Fizz"); 
+			}
+			else if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d ", x);
+			}
 		}
 	}
 	return (0);
