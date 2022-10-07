c2258311b4786494b90fbd903e88ef369e864809
TessierV <5628@holbertonstudents.com>
2022-10-07 04:40:04 -0700

    testing 9 buzz

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - print ligne
+ *@size: is the variable
+ *
+ *Description: print a square
+ *Return: /n
+ */
+void print_triangle(int size)
+{
+	int i;
+	int r;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (r = 0; r < size; r++)
+			{
+				putchar(35);
+			}
+			putchar(46);
+			putchar('\n');
+		}
+	}
+}
