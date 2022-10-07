e59f54d308eb9b1add8877434247027b02266d2c
TessierV <5628@holbertonstudents.com>
2022-10-07 00:51:57 -0700

    testing 3print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplication
+ * @a: its the variable
+ * @b: its the variable
+ *
+ *Description: multiplication
+ *Return: the result of the the multiplication
+ */
+void print_numbers(void)
+{
+	int n = 0;
+	for(n = 0; n < 9; n++)
+	{
+		_putchar(n);
+		_putchar('/n');
+	}
+}
