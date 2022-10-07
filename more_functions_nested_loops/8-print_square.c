db0379788297576244502193ecab610cd70c3f8b
TessierV <5628@holbertonstudents.com>
2022-10-07 02:45:56 -0700

    testing 8 script

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - print ligne
+ *@size: is the variable
+ *
+ *Description: print a square
+ *Return: /n
+ */
+void print_square(int size)
+{
+	int i;
+	int r;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (r = 0; r < i; r++)
+			{
+				putchar(35);
+			}
+			putchar(35);
+			putchar('\n');
+		}
+	}
+}
