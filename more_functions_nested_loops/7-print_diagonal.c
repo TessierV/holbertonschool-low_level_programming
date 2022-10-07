ff19c07537f0728e6a29363ea9e39f3cea932ec8
TessierV <5628@holbertonstudents.com>
2022-10-07 02:15:59 -0700

    testing 7 script

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print ligne
+ *@n: is the variable
+ *
+ *Description: print a ligne with a variable
+ *Return: /n
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar(92);
+	}
+	putchar('\n');
+}
