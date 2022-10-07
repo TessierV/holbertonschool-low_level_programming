06d2138be8774f7b22f0a023aeb5c09bbaefc55b
TessierV <5628@holbertonstudents.com>
2022-10-07 01:21:49 -0700

    testing 5more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14
+ *
+ *Description: print 0 - 14 x 10
+ *Return: the result
+ */
+void more_numbers(void)
+{
+	int n, x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for ( n = 48; n <'14'; n++)
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+}
