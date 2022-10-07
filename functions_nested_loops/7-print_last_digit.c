f6777efeb22c090dd28bd0b74e5f35731f614755
TessierV <5628@holbertonstudents.com>
2022-10-07 12:46:05 -0700

    reajust

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@ int print_last_digit(int r)
 {
 	int last_digit;
 
-	if (r => 0)
+	if (r > 0)
 	{
 		last_digit = r % 10;
 		_putchar('0' + last_digit);
@@ -17,7 +17,8 @@ int print_last_digit(int r)
 	else
 	{
 		last_digit = r % 10;
+		last_digit = -last_digit
 		_putchar('0' + last_digit);
-		return (-last_digit);
+		return (last_digit);
 	}
 }
