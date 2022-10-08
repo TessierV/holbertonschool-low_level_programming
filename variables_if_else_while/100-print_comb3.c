66524e4144ff09953f98ce893625d53f99910d40
TessierV <5628@holbertonstudents.com>
2022-10-08 10:35:40 -0700

    test

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,14 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
-		{
-			if (i != j){
-				putchar(',');
-				putchar(' ');
-			}
-		}
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
