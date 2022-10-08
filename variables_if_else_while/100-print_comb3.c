2db280ba470dd4389e04768e0657bd3e7c4cf828
TessierV <5628@holbertonstudents.com>
2022-10-08 10:59:10 -0700

    test

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (j = '0'; j < '10'; j++)
+		for (j = '0'; j < '9'; j++)
 		{
 			if (i % 10 < j % 10)
 			{
