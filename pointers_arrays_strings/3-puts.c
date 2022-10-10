f1e58be7bb7d88406a2f32e38ffb0b56a92073aa
TessierV <5628@holbertonstudents.com>
2022-10-10 04:18:08 -0700

    script for 3-puts - try to fix _putchar

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - return the string
+ * @s: pointer
+ * Return: \n
+ */
+void _puts(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+		_putchar(str[s]);
+		s++;
+	}
+	_putchar('\n');
+}
