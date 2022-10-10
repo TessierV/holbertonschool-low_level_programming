ac881304851d77b8c74d14aa8f67fe2801496fec
TessierV <5628@holbertonstudents.com>
2022-10-10 03:35:03 -0700

    script for 2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - search the size
+ * @char : pointer
+ * @count : pointer
+ */
+int _strlen(char *s)
+{
+	unsigned int count = 0;
+	while(*s!='\0')
+	{
+		count++;
+			s++;
+	}
+	return count;
+}
