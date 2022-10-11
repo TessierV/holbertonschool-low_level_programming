75d953f923911f3a517b2a75208317f1da2a098c
TessierV <5628@holbertonstudents.com>
2022-10-11 01:33:08 -0700

    betty fixing

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,16 @@
 void puts_half(char *str)
 {
 	int s, i, j;
-	
 	s = 0;
 
 	while (str[s] != '\0')
-	{	
+	{
 		s++;
 	}
 	j = s;
 	i = j / 2;
-	
 	for (; i <= j; i++)
-	{ 
+	{
 		if ( str[i] != '\0')
 		{
 			_putchar(str[i]);
