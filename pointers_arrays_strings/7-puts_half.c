86cd27b381832ae7f5d4bd1f10c2e4cf39d26f69
TessierV <5628@holbertonstudents.com>
2022-10-11 01:10:03 -0700

    script for 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - return the half
+ * @str: pointer
+ *
+ * Return: \n
+ */
+void puts_half(char *str)
+{
+	int s,i, j;
+	s = 0;
+
+	while (str[s] != '\0')
+	{	
+		s++;
+	}
+	j = s;
+	i = j / 2;
+	while (i <= j)
+	{ 
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
