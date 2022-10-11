4c97773d564fa067f764e373732d05be6b7f5bf4
TessierV <5628@holbertonstudents.com>
2022-10-11 06:06:21 -0700

    script 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,14 @@
  * @src: pointer
  * @dest: pointer
  *
- * Return: \n
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-
 	for (i = 0; *(src + i) != '\0'; i++)
 	{
 		dest[i] = *(src + i);
 	}
-	dest[i] = '\0';
 	return (dest);
 }
