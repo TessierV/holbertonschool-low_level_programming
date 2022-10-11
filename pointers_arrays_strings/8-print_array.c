b0d2be7772641de3939a9264956f9a8c1dd9c2b6
TessierV <5628@holbertonstudents.com>
2022-10-11 04:41:35 -0700

    script for 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - print an array
+ * @n: is the number of the element
+ *
+ * Return: \n
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+}
