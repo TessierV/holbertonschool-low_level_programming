418c3f123c75f6f726493f959615002c2212fb26
TessierV <5628@holbertonstudents.com>
2022-10-12 02:06:59 -0700

    restructuration of the directory for more clarity

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - read
+ *@str: pointer
+ *@dest: pointer
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while ((dest[i] = src[j]) != '\0')
+	{
+		i++;
+		j++;
+	}
+	return (dest);
+}
