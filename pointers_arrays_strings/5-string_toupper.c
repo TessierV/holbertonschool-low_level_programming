823e6233dce8ae32a314d73360bfc58b13fd12d5
TessierV <5628@holbertonstudents.com>
2022-10-12 01:03:44 -0700

    betty

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * string_toupper - uppercase all letter from a string
+ *@str: pointer
  *
  * Return: \n
 */
 char *string_toupper(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
