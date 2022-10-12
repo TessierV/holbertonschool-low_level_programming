1b8c40cb18f89dc836583baeeb498bd0e9bc54db
TessierV <5628@holbertonstudents.com>
2022-10-12 01:02:33 -0700

    script for string to upper

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+/*Project 1 - Pointers, arrays and strings*/
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,5 +15,23 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+/*Advanced project 1 - 2 tasks*/
 int _atoi(char *s);
+/**/
+/*Project 2 - More pointers, arrays and string*/
+/*char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);*/
+char *string_toupper(char *str);
+/*char *cap_string(char *);
+char *leet(char *);*/
+/*Advanced project 2 - 5 tasks*/
+/*char *rot13(char *);
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+*/
 #endif
