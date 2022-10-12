c20d688c65016584b3f731cb15f42b11bef729bc
TessierV <5628@holbertonstudents.com>
2022-10-12 04:28:53 -0700

    fixing the issu

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
 	while (i < n)
 	{
-		while ((dest[i] = src[i]) != '\0')
-		{
-			i++;
-		}
-		while (dest[i] != '\0')
-		{
-			i++;
-		}
+		dest[i] = '\0';
+		i++;
 	}
 	return(dest);
 }
