7323a788712ac617bcb0e53466ca6a3742478810
TessierV <5628@holbertonstudents.com>
2022-10-12 04:18:12 -0700

    try

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - read
+ *@dest: pointer
+ *@src: pointer
+ *@n: variable
+ *
+ * Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		while ((dest[i] = src[i]) != '\0')
+		{
+			i++;
+		}
+		while (dest[i] != '\0')
+		{
+			i++;
+		}
+		return(dest);
+	}
+}
