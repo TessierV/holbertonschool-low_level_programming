b36bb15dacb1003a1dbd08e2849e1ff1b68de7b3
TessierV <5628@holbertonstudents.com>
2022-10-12 01:21:46 -0700

    script for strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while ((dest[i] = src[i]))
+	while ((dest[i] = src[j]))
 	{
-		i++;
+		j++;
 	}
 	return (dest);
 }
