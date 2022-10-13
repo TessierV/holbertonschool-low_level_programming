2258c2366005824adbd887758f9ff7c6949569ea
TessierV <5628@holbertonstudents.com>
2022-10-13 04:43:36 -0700

    adjustment for first caracter

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
 				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
 				str[i - 1] == 41 || str[i - 1] == 123 ||
-				str[i - 1] == 125) && (str[i] >= 97 && str[i] <= 122))
+				str[i - 1] == 125) && (str[i] == 0 && str[i] >= 97 && str[i] <= 122))
 		{
 			str[i] = str[i] - 32;
 		}
