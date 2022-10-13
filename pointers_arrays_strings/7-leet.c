971252ecdbb5c8e606bea23f95bcdb8366fb6f2f
TessierV <5628@holbertonstudents.com>
2022-10-13 09:30:23 -0700

    test

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encode 
+ * @s: pointer
+ *
+ * Return: @s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *alpha = "aAeEoOtTlL";
+	char *num = "4433007711";
+
+	i = 0; 
+	while (s[i] != '\0')
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (alpha[j] == s[i])
+			{
+				s[i] = num[j];
+			}
+		} 
+		i++;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,7 +26,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
-/*char *leet(char *);*/
+char *leet(char *s);
 /*Advanced project 2 - 5 tasks*/
 /*char *rot13(char *);
 void print_number(int n);
