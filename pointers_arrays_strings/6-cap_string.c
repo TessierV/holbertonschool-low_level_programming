1ada4a7fa2e7f0895b1c74cfc7536b2a1d561c5b
TessierV <5628@holbertonstudents.com>
2022-10-13 04:51:21 -0700

    adjustment for first caracter

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 	
 	for (i = 0; str[i]; i++)
 	{
-		if((str[i - 1] == ' ' || str[i - 1] == "\t" ||str[i - 1] == "\n" ||
+		if((str[i - 1] == ' ' || str[i - 1] == '\t' ||str[i - 1] == '\n' ||
 				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
 				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
 				str[i - 1] == 41 || str[i - 1] == 123 || str[i] == 2 ||
