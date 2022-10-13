1e811cb26f4a2d49b792b52dbc6ac9a0e1726074
TessierV <5628@holbertonstudents.com>
2022-10-13 05:04:57 -0700

    adjustment for first caracter

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,21 @@
 char *cap_string(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i]; i++)
 	{
-		if((str[i - 1] == ' ' || str[i - 1] == '\t' ||str[i - 1] == '\n' ||
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
 				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
-				str[i - 1] == 41 || str[i - 1] == 123 || 
-				str[i - 1] == 125) && (i == 0 && str[i] >= 97 && str[i] <= 122))
+				str[i - 1] == 41 || str[i - 1] == 123 ||
+				str[i - 1] == 125) && (str[i] >= 97 && str[i] <= 122))
+		{
+			str[i] = str[i] - 32;
+		}
+		else if (i == 0 && (str[i] >= 97 && str[i] <= 122))
 		{
 			str[i] = str[i] - 32;
 		}
 	}
-	return(str);
+	return (str);
 }
