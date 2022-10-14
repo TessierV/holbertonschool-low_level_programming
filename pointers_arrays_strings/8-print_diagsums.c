0ad15840c55a99f716ec3c9ffe3d4e7bcb0b4d9f
TessierV <5628@holbertonstudents.com>
2022-10-14 05:49:36 -0700

    script 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagsums - print recursive way a multi dimensional array
+ * @a: array
+ * size: array
+ *
+ * Return: print
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, front, back;
+	
+	front = 0;
+
+	for (i = 0; i < size*size; i = i + (size + 1))
+	{
+		front = front + a[i];
+
+	}
+	for (i = size -1; i < size*size -1; i = i + (size - 1))
+	{
+		back = back + a[i];
+	}
+	printf("%d, %d\n",  front, back);
+}
