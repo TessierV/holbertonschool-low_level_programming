1ffe120dc210cc9ceb050db097eb734a53dc02c9
TessierV <5628@holbertonstudents.com>
2022-10-14 05:55:54 -0700

    script 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,13 +11,14 @@ void print_diagsums(int *a, int size)
 	int i, front, back;
 	
 	front = 0;
-	for (i = 0; i < size*size; i = i + (size + 1))
+
+	for (i = 0; i < size * size; i = i + (size + 1))
 	{
 		front = front + a[i];
 
 	}
 	back = 0;
-	for (i = size -1; i < size*size -1; i = i + (size - 1))
+	for (i = size - 1; i < size * size - 1; i = i + (size - 1))
 	{
 		back = back + a[i];
 	}
