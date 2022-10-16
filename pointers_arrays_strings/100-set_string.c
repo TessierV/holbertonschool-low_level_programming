377165701038532e881915d812251a493e55cbe9
TessierV <5628@holbertonstudents.com>
2022-10-16 10:37:19 -0700

    fixing betty

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * set_string 
- * @**s: double pointer
- * @*to: pointer
+ * set_string - set a value of a char pointer
+ * @s: double pointer
+ * @to: char pointer
  *
  */
 void set_string(char **s, char *to)
