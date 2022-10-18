97fb6cdb11fa61bacff9451cde6e650fb0d0f31c
TessierV <5628@holbertonstudents.com>
2022-10-18 04:16:00 -0700

    script 0 for recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - recursion
+ * @s: pointer
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar(10);
+	return;
+}
---
+++ recursion/README.md
@@ -0,0 +1,5 @@
+# holbertonschool-low_level_programming
+holbertonschool-slow_level_programming, learn how to use C language
+
+1- hello_world :
+gcc/printf (3)/puts/putchar
