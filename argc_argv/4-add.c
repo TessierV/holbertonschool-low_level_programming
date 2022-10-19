dd411c55ff1c88adcd75c1ff78d658598b0f2e50
TessierV <5628@holbertonstudents.com>
2022-10-19 07:25:13 -0700

    script 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, j, sum;
+
+	sum = 0;
+	a = 1;
+
+	for (; a < argc; a++)
+	{
+		j = 0;
+
+		while (argv[a][j] != '\0')
+		{
+
+			if (!isdigit(argv[a][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
