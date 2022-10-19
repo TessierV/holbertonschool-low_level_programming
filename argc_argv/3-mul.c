af5724be805774c17f0bddd449fe65d8d0f9cef2
TessierV <5628@holbertonstudents.com>
2022-10-19 04:03:12 -0700

    test

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print	all arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+		return (0);
+	}
+}
