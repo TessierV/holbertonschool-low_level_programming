d584355ec07d21c73789f51fc712d2d5bc70d39f
TessierV <5628@holbertonstudents.com>
2022-10-20 02:13:44 -0700

    script

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,22 +8,22 @@
 
 int check_prime(int i, int j)
 {
-	if (i <= 0 || i == 1 || i % j == 0)
+	if (i <= 0 || i % j ==0 || i == 1)
 	{
 		return (0);
-	} 
-	else
+	}
+	else if (i > j / 2)
 	{
 		return (1);
 	}
+	else 
+	{
 	return (check_prime(i, j + 1 ));
+	}
 } 
 
 int is_prime_number(int n)
 {
-	if (n == 2)
-	{
-		return (1);
-	}
-	return (check_prime(2, n));
+	int a = 2;
+	return (check_prime(n, a));
 }
