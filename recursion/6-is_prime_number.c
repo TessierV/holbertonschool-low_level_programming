e4ca3ed1900636e50c73fd6c27d6c5e56e58c8cf
TessierV <5628@holbertonstudents.com>
2022-10-20 02:38:53 -0700

    betty + script

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * is_prime_number - recursion
- * @n: variable
+ * check_prime - if its a prime
+ * @i: variable
+ * @j: variable
  *
  * Return: 0 or 1
  */
 
 int check_prime(int i, int j)
 {
-	if (i <= 0 || i % j ==0 || i == 1)
+	if (i <= 0 || i % j == 0 || i == 1)
 	{
 		return (0);
 	}
@@ -16,14 +17,21 @@ int check_prime(int i, int j)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
-	return (check_prime(i, j + 1 ));
+	return (check_prime(i, j + 1));
 	}
-} 
+}
 
+/**
+ * is_prime_number - prime number function
+ *
+ * @n: variable
+ * Return: the check_prime
+ */
 int is_prime_number(int n)
 {
 	int a = 2;
+
 	return (check_prime(n, a));
 }
