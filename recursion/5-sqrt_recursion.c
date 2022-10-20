83c72098f21a7f2c8cf5a323a00184ccd4e03592
TessierV <5628@holbertonstudents.com>
2022-10-20 02:58:39 -0700

    script

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * check_recursion - if its a prime
+ * @i: variable
+ * @j: variable
+ *
+ * Return: 0 or 1
+ */
+
+int check_recursion(int i, int j)
+{
+	if (j * j == i)
+	{
+		return (j);
+	}
+	else if (j > i / 2 || i <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+	return (check_recursion(i, j + 1));
+	}
+}
+
+/**
+ * is_prime_number - prime number function
+ *
+ * @n: variable
+ * Return: the check_prime
+ */
+int _sqrt_recursion(int n)
+{
+	int a = 1;
+
+	return (check_recursion(n, a));
+}
