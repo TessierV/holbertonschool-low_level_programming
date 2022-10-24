93097b806bf79b917592ef3aded520b964dde583
TessierV <5628@holbertonstudents.com>
2022-10-24 11:09:32 -0700

    test for reduce line

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -26,23 +26,21 @@ char *str_concat(char *s1, char *s2)
 	s = malloc((len) + 1);
 	if (s == NULL)
 	{
-		return ("");
+		return (NULL);
 	}
 	i = 0;
 	if (s1 != NULL)
 	{
-		for (j = 0; s1[j]; j++)
+		for (j = 0; s1[j]; j++, i++)
 		{
 			s[i] = s1[j];
-			i++;
 		}
 	}
 	if (s2 != NULL)
 	{
-		for (k = 0; s2[k]; k++)
+		for (k = 0; s2[k]; k++, i++)
 		{
 			s[i] = s2[k];
-			i++;
 		}
 	}
 	return (s);
