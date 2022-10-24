90f6c8f89c2245b62c63a9f4a779bba24fea69d5
TessierV <5628@holbertonstudents.com>
2022-10-24 11:02:11 -0700

    test for remove some line

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,14 +18,6 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 		s2 = "";
 	}
-	if (s1 != NULL && s2 == NULL)
-	{
-		len = strlen(s1);
-	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		len = strlen(s2);
-	}
 	else
 	{
 		len = strlen(s1);
@@ -37,22 +29,21 @@ char *str_concat(char *s1, char *s2)
 		return ("");
 	}
 	i = 0;
-	if (s1 != NULL)
-	{
+	/*if (s1 != NULL)
+	{*/
 		for (j = 0; s1[j]; j++)
 		{
 			s[i] = s1[j];
 			i++;
 		}
-	}
+	/*}
 	if (s2 != NULL)
-	{
+	{*/
 		for (k = 0; s2[k]; k++)
 		{
 			s[i] = s2[k];
 			i++;
 		}
-	}
+	/*}*/
 	return (s);
-	free(s);
 }
