28ee6cb09b975851ecbeb5a56480142c9da270a8
TessierV <5628@holbertonstudents.com>
2022-10-24 06:19:20 -0700

    script 2concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * str_concat - concat two strings
+ *
+ * @s1: string
+ * @s2: string 2
+ * Return: NULL or return a pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, j, k, len;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 != NULL && s2 == NULL)
+	{
+		len = strlen(s1);
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		len = strlen(s2);
+	}
+	else
+	{
+		len = strlen(s1);
+		len = len + strlen(s2);
+	}
+	s = malloc((len) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (s1[j])
+	{
+		s[i] = s1[j];
+		j++;
+		i++;
+	}
+	k = 0;
+	while (s2[k])
+	{
+		s[i] = s2[k];
+		k++;
+		i++;
+	}
+	return (s);
+}
