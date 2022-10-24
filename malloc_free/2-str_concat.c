fb433937681e49c6d2bcdc752516e7897cc4ec06
TessierV <5628@holbertonstudents.com>
2022-10-24 11:46:46 -0700

    Null fixe

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,12 +31,12 @@ char *str_concat(char *s1, char *s2)
 	}
 	i = 0;
 	if (s1 != NULL)
-		for (j = 0; s1[j]; j++, i++)
+		for (j = 0; s1[j] != '\0'; j++, i++)
 		{
 			s[i] = s1[j];
 		}
 	if (s2 != NULL)
-		for (k = 0; s2[k]; k++, i++)
+		for (k = 0; s2[k] != '\0'; k++, i++)
 		{
 			s[i] = s2[k];
 		}
