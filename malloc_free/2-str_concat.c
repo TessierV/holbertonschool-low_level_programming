949b38f325f0f89113ba5fe0db9ab39c5600bba7
TessierV <5628@holbertonstudents.com>
2022-10-25 00:28:36 -0700

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,15 +15,21 @@ char *str_concat(char *s1, char *s2)
 
 
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == NULL && s2 == NULL) 
+	{
+		return (NULL);
+	}/*
 	if (s1 == NULL)
 		{
 			s1 = "";
-			if (s1 == NULL && s2 == NULL)
-				return (NULL);
 		}
 	if (s2 == NULL)
-		s2 = "";
-	else
+		s2 = "";*/
+	if (s1 != NULL && s1 != NULL)
 	{
 		len = strlen(s1);
 		len = len + strlen(s2);
