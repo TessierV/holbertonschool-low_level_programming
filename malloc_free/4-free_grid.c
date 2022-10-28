c069cd2783db03a6b502946082a62894f2580582
TessierV <5628@holbertonstudents.com>
2022-10-28 09:09:17 -0700

    script 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * free_grid - free grid from previous project
+ * @grid: pointer 
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
