d62f5bccf40bda48d6a7efa3726d5e71d1fe80c0
TessierV <5628@holbertonstudents.com>
2022-10-28 09:11:19 -0700

    free adjust + betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,15 +2,17 @@
 /**
  *
  * free_grid - free grid from previous project
- * @grid: pointer 
+ * @grid: pointer
  * @height: height of the grid
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
+	free(grid);
 }
