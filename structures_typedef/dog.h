6affec8f9f974d85980d7b074d5a0fd0df33d99d
TessierV <5628@holbertonstudents.com>
2022-10-31 03:06:14 -0700

    test

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog
+#define dog
+/**
+ * dog - create a struct name dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ *
+ *Description: define a new type struct dog
+ */
+
+typedef struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}my_dog;
+
+#endif
