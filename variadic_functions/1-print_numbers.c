7fc7e6c88fb4c3a43d43f0d53c517a6434bf1efa
TessierV <5628@holbertonstudents.com>
2022-11-04 01:22:12 -0700

    script 1&2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print all numbers only
+ * @n: unsigned arguments
+ * Return: 0 or print all numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list intarg;
+
+	va_start(intarg, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(intarg, int));
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(intarg);
+}
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings
+ * @separator: delimit
+ * @n: unsigned arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *arg;
+	va_list intarg;
+
+
+	va_start(intarg, n);
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(intarg, char*);
+		if (intarg != NULL)
+		{
+			printf("%s", arg);
+		}
+		else
+		{
+			printf("\n");
+		}
+		if (separator != NULL || i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(intarg);
+}
