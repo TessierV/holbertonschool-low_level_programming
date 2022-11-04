f6055e060bacc2adb75f160fc98e05429f3bc2f8
TessierV <5628@holbertonstudents.com>
2022-11-04 05:09:11 -0700

    betty and deleting commentary print all function wrong position

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,45 +1,8 @@
 #include "variadic_functions.h"
 
 /**
- *print_all - print all
- *@format - input
- */
-/*
-void print_all(const char * const format, ...)
-{
-	v_types print[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-	};
-
-	va_list arguments;
-	va_start(arguments, format);
-
-	int i, j;
-	j = 0;
-
-	while (format[j])
-	{
-		i = 0;
-		while (print[i].all)
-		{
-			if (print[i].all == *format)
-			{
-				print[i].func(arguments);
-			}
-			i++;
-		}
-		j++;
-	}
-	printf("\n");
-	va_end(arguments);
-}
-*/
-/**
- * print_char - character
- * @arguments: list of arguments
+ *print_char - print char
+ *@arguments: arguments
  */
 void print_char(va_list arguments)
 {
@@ -52,7 +15,7 @@ void print_char(va_list arguments)
  */
 void print_int(va_list arguments)
 {
-	printf("%d", va_arg(arguments, int));			
+	printf("%d", va_arg(arguments, int));
 }
 
 /**
@@ -73,7 +36,10 @@ void print_string(va_list arguments)
 {
 	printf("%s", va_arg(arguments, char*));
 }
-
+/**
+ * print_all - print all arguments
+ * @format: input
+ */
 void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
@@ -85,6 +51,7 @@ void print_all(const char * const format, ...)
 	};
 
 	va_list arguments;
+
 	va_start(arguments, format);
 
 	j = 0;
