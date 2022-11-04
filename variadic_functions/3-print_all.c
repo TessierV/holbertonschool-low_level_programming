cfcab3281cc65dc9cfd05d4940bbb099dae26acf
TessierV <5628@holbertonstudents.com>
2022-11-04 05:04:16 -0700

    test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,15 +1,17 @@
 #include "variadic_functions.h"
+
 /**
  *print_all - print all
  *@format - input
  */
+/*
 void print_all(const char * const format, ...)
 {
 	v_types print[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string},
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
 	};
 
 	va_list arguments;
@@ -17,7 +19,7 @@ void print_all(const char * const format, ...)
 
 	int i, j;
 	j = 0;
-	
+
 	while (format[j])
 	{
 		i = 0;
@@ -34,7 +36,7 @@ void print_all(const char * const format, ...)
 	printf("\n");
 	va_end(arguments);
 }
-
+*/
 /**
  * print_char - character
  * @arguments: list of arguments
@@ -72,3 +74,34 @@ void print_string(va_list arguments)
 	printf("%s", va_arg(arguments, char*));
 }
 
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	v_types print[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+	};
+
+	va_list arguments;
+	va_start(arguments, format);
+
+	j = 0;
+
+	while (format[j])
+	{
+		i = 0;
+		while (print[i].all)
+		{
+			if (print[i].all == *format)
+			{
+				print[i].func(arguments);
+			}
+			i++;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(arguments);
+}
