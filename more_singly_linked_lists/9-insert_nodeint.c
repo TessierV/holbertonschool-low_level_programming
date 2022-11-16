43659e42f8aa705588eb0ada542f46d78189208a
TessierV <5628@holbertonstudents.com>
2022-11-16 04:43:58 -0800

    script 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_nodeint - add new node of somewhere in the index list
+ * @head: list
+ * @idx: place in the index
+ * @n: int
+ * Return:newNode
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head, *newNode;
+	unsigned int i;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = n;
+	if (idx == 0)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		for (i = 0; i < idx - 1; i++)
+		{
+			if (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+		}
+		newNode->next = temp->next;
+		temp->next = newNode;
+	}
+	return (newNode);
+}
