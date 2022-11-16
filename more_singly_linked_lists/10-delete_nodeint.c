4f23fe5c9dcd5a528225ea147cb0db4528f2b18b
TessierV <5628@holbertonstudents.com>
2022-11-16 06:00:29 -0800

    fixing

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		for (i = 0; i < index - 1; i++)
 		{
-		i	temp = temp->next;
+			temp = temp->next;
 		}
 		rmNode = temp->next;
 		temp->next = rmNode->next;
