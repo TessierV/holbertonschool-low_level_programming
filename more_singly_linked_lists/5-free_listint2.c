1bf13fa34a0ec1835ca9aae6af8de9a78903c8f3
TessierV <5628@holbertonstudents.com>
2022-11-16 01:40:31 -0800

    move the comparison

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	
+
 	if (head == NULL)
 	{
 		return;
 	}
-	temp = *head;
 	while (*head != NULL)
 	{
+		temp = *head;
 		*head = temp->next;
 		free(temp);
 	}
