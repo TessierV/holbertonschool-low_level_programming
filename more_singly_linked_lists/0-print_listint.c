0c3a4911d6ed2e5f67858a6e280a617f1f6fc3f0
TessierV <5628@holbertonstudents.com>
2022-11-16 00:31:13 -0800

    script 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * print_listint - print all the element og the linked listint
+ * @h: pointer of the list
+ * Return: i
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+			printf("%d\n", h->n);
+			h = h->next;
+	} return (i);
+}
