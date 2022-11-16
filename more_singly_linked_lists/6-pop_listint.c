ccf4070cb65a6cd839534f4e316085ea8053a0a5
TessierV <5628@holbertonstudents.com>
2022-11-16 01:52:53 -0800

    script 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - this delete the head node
+ * @head: pointer of the list
+ *
+ * Return: the new headnode
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	i = (*head)->n;
+	*head = temp->next;
+	free(temp);
+	return (i);
+}
