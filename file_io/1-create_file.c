6c3ca3bcf56eed5417ed1124385facd862821c79
TessierV <5628@holbertonstudents.com>
2022-11-22 15:55:07 -0800

    take out a wrong open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,9 +9,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f, w, len;
-
-	f = open(filename, O_CREAT | O_RDWR | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (f == -1 || filename == NULL)
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
 	{
 		return (-1);
 	}
@@ -19,9 +22,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-	/*length = strlen(text_content);*/
+	len = strlen(text_content);
 	w = write(f, text_content, len);
 	if (w == -1)
 		return (-1);
