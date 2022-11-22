ae7dc11148a1b34a07c3504684e1755531d553f2
TessierV <5628@holbertonstudents.com>
2022-11-22 03:08:36 -0800

    script 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_dnodeint - of a linked list
+ * @head: list
+ * @n: int
+ * Return:newNode
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
