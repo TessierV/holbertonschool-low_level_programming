2db89a75724b84e4319fd6fec46822dacb20cec6
TessierV <5628@holbertonstudents.com>
2022-11-22 05:13:27 -0800

    test

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	if (temp->next == NULL)
+	if (!temp->next)
 	{
 		temp->prev->next = NULL;
 		free(temp);
