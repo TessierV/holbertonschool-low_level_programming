65b02c95ec1d9814b2795e2f58b9e9c8b5da9bc7
TessierV <5628@holbertonstudents.com>
2022-11-22 14:56:38 -0800

    fixing issu nul

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,14 +8,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int f;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_WRONLY | O_TRUNC, S_IWRITE | S_IREAD);
-	if (fd == -1)
+	f = open(filename, O_RDWR | O_CREAT | O_WRONLY | O_TRUNC, S_IWRITE | S_IREAD);
+	if (f == -1)
 	{
 		return (-1);
 	}
@@ -23,14 +23,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		text_content = "";
-		close(fd);
-		return (1);
 	}
 	if (text_content)
 	{
-		write(fd, text_content, strlen(text_content));
+		write(f, text_content, strlen(text_content));
 	}
-	close(fd);
+	close(f);
 	return (1);
 }
 
