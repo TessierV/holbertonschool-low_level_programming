a92d5beb73467c00ef54572d8b4c8029723b5486
TessierV <5628@holbertonstudents.com>
2022-11-22 15:26:09 -0800

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,29 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f, w, length;
+	int f, w, len;
 
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	f = open(filename, O_CREAT | O_RDWR | O_WRONLY | O_TRUNC, S_IWRITE | S_IREAD);
-	if (f == -1)
+	f = open(filename, O_CREAT | O_RDWR | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1 || filename == NULL)
 	{
 		return (-1);
 	}
 	if (text_content == NULL)
 	{
 		text_content = "";
-		close(f);
-		return (1);
 	}
-	length = strlen(text_content);
-	w = write(f, text_content, length);
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	/*length = strlen(text_content);*/
+	w = write(f, text_content, len);
 	if (w == -1)
-	{
 		return (-1);
-	}
 	close(f);
 	return (1);
 }
