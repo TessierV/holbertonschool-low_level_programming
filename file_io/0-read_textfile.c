519ba8d3792f995b518cd12bd00d0019f76e5b08
TessierV <5628@holbertonstudents.com>
2022-11-22 14:00:29 -0800

    script 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/*
+ * read_textfile - function that reads a text file and prints it
+ * @filename: name
+ * @letters: content
+ *
+ * Return: the content
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * (letters));
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (w);
+}
+
---
+++ file_io/README.md
@@ -0,0 +1,6 @@
+#C - File I/O:
+
+##Mandatory (3 tasks):
+0. Tread lightly, she is near
+1. Under the snow
+2. Speak gently, she can hear
