4bea1082a25584bd15ad55e3ad1425d8ca24baa9
TessierV <5628@holbertonstudents.com>
2022-11-22 06:45:50 -0800

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *newNode;
 	unsigned int i;
+	
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL || temp == NULL)
 		return (NULL);
 	newNode->n = n;
+	temp = *h;
 	if (idx == 0 || *h == NULL)
 	{
 		newNode->next = NULL;
-		newNode->next = *h;
-		newNode->prev = NULL;
+		newNode->next = temp;
+		temp->prev = newNode;
 		*h = newNode;
 		return (newNode);
 	}
-	else
+	for (i = 0; i < idx -1; i++)
 	{
-		temp = *h;
-		for (i = 0; i < idx -1; i++)
-		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (NULL);
-		}
-		if (temp->next == NULL)
-		{
-			temp->prev->next = NULL;
-			free(temp);
-			return (temp);
-		}
-		newNode->next = temp->next;
-		newNode->prev = temp;
-		temp->next = newNode;
-		temp = newNode->next;
-		temp->prev = newNode;
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 	}
+	if (temp->next == NULL)
+	{
+		temp->prev->next = NULL;
+		free(temp);
+		return (temp);
+	}
+	newNode->next = temp->next;
+	newNode->prev = temp;
+	temp->next = newNode;
+	temp = newNode->next;
+	temp->prev = newNode;
+
 	return (newNode);
 }
