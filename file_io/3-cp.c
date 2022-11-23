4de53fc87ed4f5df119acbf99bdf4d0d10068b56
TessierV <5628@holbertonstudents.com>
2022-11-23 03:06:11 -0800

    short line and betty doc

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
- * append_text_to_file - function who append text at the end of a file
- * @filename: name
- * @text_content: content
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @ar: argument argv
  *
- * Return: 1 if true -1 if false
+ * Return: 0 in success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *ar[])
 {
 	int file_from, file_to, w, r;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = open(argv[1], O_RDONLY);
+	file_from = open(ar[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ar[1]), exit(98);
+	file_to = open(ar[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ar[2]), exit(99);
 	r = 1024;
 	while (r == 1024)
 	{
 		r = read(file_from, buffer, 1024);
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ar[1]), exit(98);
 		w = write(file_to, buffer, r);
 		if (w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ar[2]), exit(99);
 	}
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
