6135f4fecaba14fce5c6c214f864eef3337cd247
TessierV <5628@holbertonstudents.com>
2022-11-23 05:35:41 -0800

    script 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: table size
+ *
+ * Return: if fails return NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newtable;
+	unsigned long int i;
+
+	if (size > 0)
+	{
+		newtable = malloc(sizeof(hash_table_t));
+		if (newtable == NULL)
+		{
+			return (NULL);
+		}
+		newtable->array = malloc(size * sizeof(hash_table_t *));
+		if (newtable->array == NULL)
+		{
+			free(newtable);
+			return (NULL);
+		}
+		newtable->size = size;
+		for (i = 0; i < size; i++)
+		{
+			newtable->array[i] = NULL;
+		}
+		return(newtable);
+	}
+	return (NULL);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,13 @@
+#C- Hash Tables:
+
+#Mandatory - 7 tasks:
+0. >>> ht = {}
+1. djb2
+2. key->index
+3. >>> ht['betty'] = 'cool'
+4. >>> ht['betty']
+5. >>> print(ht)
+6. >>> del ht
+
+#Advanced - 1 task:
+7. $ht['Betty'] = 'Cool'
