3535d6002483373b03cb79f93af4badc7b1c133f
TessierV <5628@holbertonstudents.com>
2022-11-23 07:07:28 -0800

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,14 +15,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n =  n;
-	
+	temp = *h;
 	if (temp == NULL)
 	{
 		newNode->prev->next = NULL;
 		*h = newNode;
 		return (newNode);
 	}
-	temp = *h;
 	if (idx == 0)
 	{
 		newNode->prev = NULL;
