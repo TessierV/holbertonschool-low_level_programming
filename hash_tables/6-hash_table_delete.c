5acc907d099f315a02a560cf6635f44f7bf51cc7
TessierV <5628@holbertonstudents.com>
2022-11-25 03:10:16 -0800

    wrong emplacement

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -24,9 +24,10 @@ void hash_table_delete(hash_table_t *ht)
 				node->next = rmNode->next;
 				node = rmNode;
 			}
+			index++;
 		}
+		free(ht->array);
+		free(ht);
 	}
-	free(ht->array);
-	free(ht);
 }
 
