4692facc048d0e05419ea942b40395492e61d1d5
TessierV <5628@holbertonstudents.com>
2022-11-25 04:30:27 -0800

    script 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 		return;
 	if (ht != NULL)
 	{
-		while (index = 0; index < ht->size; index++)
+		for (index = 0; index < ht->size; index++)
 		{
 			node = ht->array[index];
 			while (node)
