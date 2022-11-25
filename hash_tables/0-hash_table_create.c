980699091e615832cc9edd54e7ba9beaed977549
TessierV <5628@holbertonstudents.com>
2022-11-25 07:08:43 -0800

    rewrite script 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,25 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newtable;
-	unsigned long int i;
 
 	if (size > 0)
 	{
 		newtable = malloc(sizeof(hash_table_t));
 		if (newtable == NULL)
 		{
+			free(newtable);
 			return (NULL);
 		}
+		newtable->size = size;
 		newtable->array = malloc(size * sizeof(hash_table_t *));
 		if (newtable->array == NULL)
 		{
 			free(newtable);
 			return (NULL);
-		}
-		newtable->size = size;
-		for (i = 0; i < size; i++)
-		{
-			newtable->array[i] = NULL;
 		}
 		return (newtable);
 	}
