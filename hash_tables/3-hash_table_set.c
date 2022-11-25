e98cce0287d56a730ff8b07b7f2c67ad83c27714
TessierV <5628@holbertonstudents.com>
2022-11-25 05:14:25 -0800

    free 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(upd->key, key) == 0)
 		{
+			free(upd->value);
 			upd->value = strdup(value);
 			return (1);
 		}
