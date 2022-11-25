2cbf8de6a031af80b0cb84cf08cdb62405b18903
TessierV <5628@holbertonstudents.com>
2022-11-25 07:11:25 -0800

    test 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 			return (NULL);
 		}
 		newtable->size = size;
-		newtable->array = malloc(size * sizeof(hash_table_t *));
+		newtable->array = malloc(size * sizeof(hash_node_t *));
 		if (newtable->array == NULL)
 		{
 			free(newtable);
