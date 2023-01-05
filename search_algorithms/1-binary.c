988daa5c8f28d849b4b6ef9855e41c3aa5ff60d7
TessierV <5628@holbertonstudents.com>
2023-01-05 01:16:52 -0800

    reajust the output on the terminal & betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,13 +10,14 @@
 int binary_search(int *array, size_t size, int value)
 {
 	int i, l, r;
+
 	l = 0;
 	r = size - 1;
 	while (l <= r)
 	{
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = l; i < r; i++)
-			printf("%d\n ", array[i]);
+			printf("%d ,", array[i]);
 		printf("%d\n", array[i]);
 		i = l + (r - l) / 2;
 		if (array[i] == value)
